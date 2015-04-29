a26d63361030acfd319aa40601e9df70320cfaff
David Banas <capn.freako@gmail.com>
2015-04-29 06:15:06 -0700

    Initial revision of ibisami_api.cpp - Added '*.swp' to .gitignore.

---
+++ src/ibisami_api.cpp
@@ -0,0 +1,12 @@
+// ibisami_api.cpp
+//
+// Original author: David Banas
+// Original date:   April 29, 2015
+//
+// Copyright (c) 2015 David Banas; all rights reserved World wide.
+//
+// Provides the API (application programming interface) to the *.SO (shared object) or *.DLL (dynamically linked library) file.
+//
+// Note: The required API for a IBIS-AMI model is defined by the IBIS standard, which is available here:
+//       http://www.eda.org/ibis/
+
