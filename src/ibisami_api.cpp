bd5d5f9289ac0cf591490f889b27670d8b958a00
David Banas <capn.freako@gmail.com>
2015-04-29 17:20:49 -0700

    Added extern "C" wrapper to ibisami_api.cpp.

--- src/ibisami_api.cpp
+++ src/ibisami_api.cpp
@@ -10,3 +10,40 @@
 // Note: The required API for a IBIS-AMI model is defined by the IBIS standard, which is available here:
 //       http://www.eda.org/ibis/
 
+extern "C" {
+// Initialization and impulse response processing.
+long AMI_Init( // Required.
+        double * impulse_matrix,
+        long     number_of_rows,
+        long     aggressors,
+        double   sample_interval,
+        double   bit_time,
+        char   * AMI_parameters_in,
+        char  ** AMI_parameters_out,
+        void  ** AMI_memory_handle,
+        char  ** msg
+     )
+{
+}
+
+`ifdef AMI_GETWAVE
+// Time domain signal processing.
+long AMI_GetWave( // Optional.
+        double * wave,
+        long     wave_size,
+        double * clock_times,
+        char  ** AMI_parameters_out,
+        void   * AMI_memory
+     )
+{
+}
+`endif
+
+// Clean-up.
+long AMI_Close( // Required.
+        void * AMI_memory
+     )
+{
+}
+}
+
