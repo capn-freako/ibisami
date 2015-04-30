0203ee81e837364ac9f1634d90db1f710aba88c0
David Banas <capn.freako@gmail.com>
2015-04-29 18:19:58 -0700

    Clean-up, after running cpplint.py.

--- src/ibisami_api.cpp
+++ src/ibisami_api.cpp
@@ -5,14 +5,15 @@
 //
 // Copyright (c) 2015 David Banas; all rights reserved World wide.
 //
-// Provides the API (application programming interface) to the *.SO (shared object) or *.DLL (dynamically linked library) file.
+// Provides the API (application programming interface) to the
+// *.SO (shared object) or *.DLL (dynamically linked library) file.
 //
-// Note: The required API for a IBIS-AMI model is defined by the IBIS standard, which is available here:
-//       http://www.eda.org/ibis/
+// Note: The required API for a IBIS-AMI model is defined by the IBIS
+//       standard, which is available here: http://www.eda.org/ibis/
 
 extern "C" {
-// Initialization and impulse response processing.
-long AMI_Init( // Required.
+// Initialization and impulse response processing. (Required)
+long AMI_Init(
         double * impulse_matrix,
         long     number_of_rows,
         long     aggressors,
@@ -22,28 +23,25 @@ long AMI_Init( // Required.
         char  ** AMI_parameters_out,
         void  ** AMI_memory_handle,
         char  ** msg
-     )
-{
+     ) {
 }
 
-`ifdef AMI_GETWAVE
-// Time domain signal processing.
-long AMI_GetWave( // Optional.
+#ifdef AMI_GETWAVE
+// Time domain signal processing. (Optional)
+long AMI_GetWave(
         double * wave,
         long     wave_size,
         double * clock_times,
         char  ** AMI_parameters_out,
         void   * AMI_memory
-     )
-{
+     ) {
 }
-`endif
+#endif
 
-// Clean-up.
-long AMI_Close( // Required.
+// Clean-up. (Required)
+long AMI_Close(
         void * AMI_memory
-     )
-{
+     ) {
 }
 }
 
